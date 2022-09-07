3fdf0a83fc141874d3e490d97c6fd38fd942b390
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-09-07 13:44:21 +0300

    Initial Commit

---
+++ ReusableSearchBar/ReusableSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableSearchBarApp.swift
+//  ReusableSearchBar
+//
+//  Created by Paul Kirnoz on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReusableSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
